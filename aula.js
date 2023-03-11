1efd59e86367ee2d9c2f8592468b09f5475171cd
Kira <kira.oliveira.web@gmail.com>
2023-03-11 00:51:13 -0300

    Par ou Impar

--- aula.js
+++ aula.js
@@ -1,29 +1,29 @@
 //VARIÁVEIS - Usar o const para casos onde não teremos variação do dado
 
-/*const nome = 'Kira'
+const nome = 'Kira'
 console.log(nome)
 
 const idade = 34
-console.log(idade)*/
+console.log(idade)
 
 //*************************** */
 
 //CONSTANTES - Usar Let para casos onde vamos ter variação do dado
 
-/*let valorIngressoAdulto = 20
+let valorIngressoAdulto = 20
 valorIngressoAdulto = 30
-console.log(valorIngressoAdulto)*/
+console.log(valorIngressoAdulto)
 
 //******************************* */
 
 //TIPOS PRIMITIVOS - VALORES
 
-/*const nome = 'Kira' // Tipo String
-const idade = 34 // Tipo Number
+const nomee = 'Kira' // Tipo String
+const idadee = 34 // Tipo Number
 const estaAprovado = true // Tipo Boolean
 const corPreferida = 'preto' // Tipo String
 const sobrenome = undefined //  Undefined (Não é usado)
-const corSelecionado = null // Para casos que a info vai ser redefinida (Redefinir um valor)*/
+const corSelecionado = null // Para casos que a info vai ser redefinida (Redefinir um valor)
 
 //***************************** */
 
@@ -37,22 +37,22 @@ const corSelecionado = null // Para casos que a info vai ser redefinida (Redefin
 
 //OBJETO
 
-/*let pessoa = {
+let pessoa = {
   nome: 'Kira',
   idade: 34,
   corPreferida: 'preto',
   estaAprovado: true
 }
 
-console.log(pessoa)*/
+console.log(pessoa)
 
 //***************************************** */
 
 //ARRAYS
 
-/*let familia = [56, 62, 34, 31, 34, 'Kira', 'Guih']
+let familia = [56, 62, 34, 31, 34, 'Kira', 'Guih']
 console.log(familia.length)
 console.log(familia[0])
 
 let nomeColega = ['Juh', 38]
-console.log(familia, nomeColega)*/
+console.log(familia, nomeColega)
