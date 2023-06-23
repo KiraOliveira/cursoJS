e874f921b49e92d6fa921049e69cef9863499329
Kira <kira.oliveira.web@gmail.com>
2023-06-23 19:01:16 -0300

    Exercício pronto

--- scripts/qa.py
+++ scripts/qa.py
@@ -1,18 +1,19 @@
 nota1 = float(input('Primeira nota: '))
 nota2 = float(input('Segunda nota: '))
 nota3 = float(input('Terceira nota: '))
-nota4 = float(input('Quarta nota: '))
 ME = 10
 
-MA = nota1 + (nota2 * 2) + (nota3 * 3) + ME / 7
+MA = ((nota1 + (nota2 * 2) + (nota3 * 3) + ME) / 7)
 
 print('Média: ', MA)
 
-if(MA <= 7.5 && MA < 9.0)
-    print('B')
-elif((MA == 6.0) AND (MA < 7.5))
+if(MA >= 9.0):
+    print('A')
+elif((MA >= 7.5) and (MA < 9.0)):
+    print('B')    
+elif((MA >= 6.0) and (MA < 7.5)):
     print('C')
-elif((MA == 4.0) AND (MA <6.0))
+elif((MA >= 4.0) and (MA < 6.0)):
     print('D')
 else:
-    print('A')
+    print('E')
